f3df1c229e5df9771a46157409c4aab0dd0591a1
Fanny <113479742+Fanny101@users.noreply.github.com>
2023-05-15 19:36:05 +0100

    images and design

---
+++ app.js
@@ -0,0 +1,22 @@
+function sings() {
+    prompt("Enter a number")
+}
+
+function greet(firstName) {
+    console.log(`Firstname is ${firstName}`)
+}
+let sum ="Fanny"
+prompt("Enter name")
+if ( prompt !== "Fanny") {
+    
+ alert("Error")
+ 
+}
+else {
+  alert("Correct name")  
+ }
+
+ function capitalize(word){
+    return word.toUppercase(0);
+}
+fanny.toUppercase
